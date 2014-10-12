75c4f4a16e487eb351d318e650c81ad4823f5936
Shaun <shaunsibu@gmail.com>
2014-10-12 14:16:34 -0600

    Update.

--- beanstalk.mk
+++ beanstalk.mk
@@ -6,4 +6,8 @@ $(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
+PRODUCT_DEVICE := m8
 PRODUCT_NAME := beanstalk_m8
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := One M8
+PRODUCT_MANUFACTURER := htc
