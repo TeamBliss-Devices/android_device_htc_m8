a1d7b28e366e5e79e09adda2e1ef3940542f308f
Jack <jackeagle102@gmail.com>
2014-10-15 16:35:37 +0530

    Blissfy

--- beanstalk.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/htc/m8/full_m8.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
-
-PRODUCT_NAME := beanstalk_m8
