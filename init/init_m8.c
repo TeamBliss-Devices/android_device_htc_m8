e85d1a0638e0882a0cb019c00de6b451e8fb745e
k5t4j5 <boogs19.kj@gmail.com>
2014-11-26 12:33:21 -0500

    Show Sprint some love for once
    Conflicts:
    	init/init_m8.c
    
    Change-Id: If8d9a5ebf56fb2fd958294ad86ee6b8111738bcf

--- init/init_m8.c
+++ init/init_m8.c
@@ -79,16 +79,25 @@ void vendor_load_properties()
         property_set("ro.ril.def.agps.mode", "6");
     } else if (strstr(bootmid, "0P6B70000")) {
         /* m8spr (m8whl) */
-        cdma_properties("1", "8", "310120", "Sprint");
-        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.2/KOT49H/326598.8:user/release-keys");
-        property_set("ro.build.description", "1.54.651.8 CL326598 release-keys");
+        cdma_properties("1", "10", "310120", "Sprint");
+        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.4/KTU84P/393205.2:user/release-keys");
+        property_set("ro.build.description", "3.30.654.2 CL393205 release-keys");
         property_set("ro.product.device", "m8spr");
         property_set("ro.build.product", "m8spr");
         property_set("telephony.sms.pseudo_multipart", "1");
+        property_set("ro.telephony.default_network", "10");
+        property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.enable.pre_r8fd=1", "1");
+        property_set("persist.radio.apm_sim_not_pwdn", "1");
+        property_set("persist.radio.snapshot_enabled", "1");
+        property_set("persist.radio.snapshot_timer", "22");
+        property_set("persist.radio.mode_pref_nv10", "1");
+        property_set("ro.ril.scan.selection.rat", "1");
+        property_set("persist.radio.always_send_plmn", "true");
         property_set("ro.ril.enable.sdr", "0");
         property_set("ro.ril.set.mtusize", "1422");
+        property_set("keyguard.no_require_sim", "1");
     } else {
         /* m8 */
         property_set("ro.build.fingerprint", "htc/htc_europe/htc_m8:4.4.2/KOT49H/325784.5:user/release-keys");
