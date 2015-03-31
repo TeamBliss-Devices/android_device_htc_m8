c3c65724ca3bbd76df9de8bbd1716f35989861d3
Shaun Eaton <shaunsibu@gmail.com>
2015-03-31 14:02:07 -0600

    Disable optimisations and GCC flags for the moment.
    Change-Id: I7937067354e4547eee62eeed65a6c358a8fb4b61

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,18 +202,18 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/htc/m8/releasetools
 BOARD_HARDWARE_CLASS := device/htc/m8/cmhw
 
 # BlissPop Config Flags
-BLISS_WIPE_CACHES := 1
-TARGET_TC_ROM := 4.8-linaro
-TARGET_TC_KERNEL := 4.8-linaro
-BLISSIFY := true
-BLISS_O3 := true
-BLISS_STRICT := true
-BLISS_GRAPHITE := false
-BLISS_KRAIT := true
-BLISS_PIPE := true
-TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+#BLISS_WIPE_CACHES := 1
+#TARGET_TC_ROM := 4.9-linaro
+#TARGET_TC_KERNEL := 5.0-linaro
+#BLISSIFY := true
+#BLISS_O3 := true
+#BLISS_STRICT := true
+#BLISS_GRAPHITE := false
+#BLISS_KRAIT := true
+#BLISS_PIPE := true
+#TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
 
 #SaberMod
--include vendor/bliss/config/sm.mk
+#-include vendor/bliss/config/sm.mk
 
